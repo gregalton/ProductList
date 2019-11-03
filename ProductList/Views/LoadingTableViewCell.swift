e90f81180adfb23c96651f48557b99c5884bfa14
Greg Alton <gregaltonmail@gmail.com>
2019-11-03 15:41:17 -0600

    Loading 20 items at a time in the background with the prefetching protocol

---
+++ ProductList/Views/LoadingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingTableViewCell.swift
+//  ProductList
+//
+//  Created by Greg Alton on 11/3/19.
+//  Copyright © 2019 Greg Alton. All rights reserved.
+//
+
+import UIKit
+
+class LoadingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
